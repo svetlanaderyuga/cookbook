6aabe001f67829de188857cf078b53efe73ddc98
svetlanaderyuga <47187610+svetlanaderyuga@users.noreply.github.com>
2019-02-03 09:22:22 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
