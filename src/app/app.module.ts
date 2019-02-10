bf1eca3816c027937d5c59910e809cbc48a4734e
Svetlana Deryuga <svetlana.deryuga@gmail.com>
2019-02-10 15:30:17 -0600

    Added scaffolding for the new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { DiscussionsComponent } from './components/discussions/discussions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+    RecipesComponent,
+    DiscussionsComponent
   ],
   imports: [
     BrowserModule,
