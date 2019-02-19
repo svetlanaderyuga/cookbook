316f1af1e0df138617e3fc65ca3184b890ac77b0
Yulia Svitukha <temp@MacBook-Pro-2.local>
2019-02-19 02:40:05 -0600

    Space is added

--- src/app/modules/mock-authors.ts
+++ src/app/modules/mock-authors.ts
@@ -4,6 +4,6 @@ export const AUTHORS: Author[] = [
   { id: 1, firstName: 'Svetlana', lastName: 'Deryuga',image:'assets/img/SD.jpg'},
   { id: 2, firstName: 'Znanna', lastName: 'Grinko',image:'assets/img/ZG.jpg'},
   { id: 3, firstName: 'Olena', lastName: 'Kovalov',image:'assets/img/OK.jpg'},
-  { id: 4, firstName: 'Yulia', lastName: 'Svitukha',image:'assets/img/YS.jpg'}
+  { id: 4,  firstName: 'Yulia', lastName: 'Svitukha',image:'assets/img/YS.jpg'}
   
 ];
